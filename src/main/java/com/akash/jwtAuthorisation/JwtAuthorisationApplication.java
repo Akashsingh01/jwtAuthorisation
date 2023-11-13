073909f2aa2d52ea02aa85e6fb8a976c384a7ebb
akashsingh01 <engg.akashsingh@gmail.com>
2023-11-13 16:04:16 +0530

    initail commit

---
+++ src/main/java/com/akash/jwtAuthorisation/JwtAuthorisationApplication.java
@@ -0,0 +1,13 @@
+package com.akash.jwtAuthorisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthorisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthorisationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/jwtAuthorisation/JwtAuthorisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.jwtAuthorisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthorisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
